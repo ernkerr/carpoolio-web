8fcb29564d8e5ac0b4b8cfafdf7917775c3f3cc5
Erin Kerr <erin.kerr17@gmail.com>
2025-06-25 21:50:52 -0700

    Add Supabase configuration and enhance app opening functionality in Carpoolio

---
+++ config.js
@@ -0,0 +1,5 @@
+const SUPABASE_CONFIG = {
+  url: "https://chpjrievgxrxswmbgjyl.supabase.co",
+  anonKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNocGpyaWV2Z3hyeHN3bWJnanlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3MDIxNDgsImV4cCI6MjA1OTI3ODE0OH0.Mt8wplXkCOtXb0bShclHgaodX06oEiX4xpMkvwsCic4",
+};
